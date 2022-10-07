00f176b20bcc47ee0cb85ca2d5ec9c96662f849e
Nikita <tesh-123@mail.ru>
2022-10-07 19:27:53 +0300

    Добавил задание по 7 семинару

---
+++ DZ7/Program.cs
@@ -0,0 +1,131 @@
+﻿//Задача 47
+/*double[,] FillMatrix(int rowsCount, int columnsCount)
+{
+    double[,] matrix = new double[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.NextDouble() * 100 + 20;
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]:f2}" + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+*/
+//Задача 50
+/*int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 10, int rightRange = 99)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = FillMatrix(4, 4);
+PrintMatrix(matrix);
+Console.WriteLine("Введите индекс строки");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца");
+int n = Convert.ToInt32(Console.ReadLine());
+void FindIndexMatrix(int[,] matrix)
+{
+    if (m >= 0 && m < matrix.GetLength(0) && n >= 0 && n < matrix.GetLength(1))
+    {
+        Console.WriteLine(matrix[m, n]);
+    }
+    else
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+}
+FindIndexMatrix(matrix);
+*/
+//Задача 52
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindArithmeticColumnMatrix(int[,] matrix)
+{
+    double arithmeticColumns = 0;
+    double sumColumns = 0;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sumColumns += matrix[i, j];
+        }
+        arithmeticColumns = sumColumns / matrix.GetLength(0);
+        Console.Write($"Сумма {j + 1} столбца = {sumColumns} ");
+        Console.WriteLine($" Среднеарифметическое = {arithmeticColumns:F2}");
+        sumColumns = 0;
+    }
+}
+
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+FindArithmeticColumnMatrix(matrix);
+
